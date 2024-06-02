62d80ea73d782b0f76c3bafc8d5faede2b96d1a5
Alaaddin <alaaiddin.albakkour@gmail.com>
2024-06-02 23:55:10 +0200

    Add project files.

---
+++ Dnc.Common.Razor/Modal/DncModalComponent.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dnc.Common.Razor.Modal
+{
+    public class DncModalComponent<TItem> : ComponentBase
+    {
+        [Parameter] public RenderFragment<TItem> HeaderTemplate { get; set; }
+        [Parameter] public RenderFragment<TItem> BodyTemplate { get; set; }
+        [Parameter] public RenderFragment<TItem> FooterTemplate { get; set; }
+
+        [Parameter] public EventCallback<TItem> OnSubmit { get; set; }
+        [Parameter] public EventCallback<TItem> OnShow { get; set; }
+        [Parameter] public string Size { get; set; }
+
+        public EditContext EditContext { get; protected set; }
+        protected TItem Item { get; set; }
+
+        protected bool IsVisible { get; set; }
+        protected string ModalSize { get; set; } = string.Empty;
+
+        public void SetEditContext(EditContext editContext)
+        {
+            EditContext = editContext;
+        }
+
+        public async Task Show(TItem item = default)
+        {
+            ModalSize = Size switch
+            {
+                "Small" => "modal-sm",
+                "Medium" => string.Empty,
+                "Larg" => "modal-lg",
+                "ExtraLarg" => "modal-xl",
+                _ => string.Empty,
+            };
+
+            IsVisible = true;
+            Item = item;
+
+            var task = OnShow.InvokeAsync(Item);
+            if (task != null && !task.IsCompleted)
+            {
+                StateHasChanged();
+                await task;
+            }
+
+            EditContext ??= new EditContext(new { });
+
+            StateHasChanged();
+        }
+        public async Task HandleSubmit()
+        {
+            await OnSubmit.InvokeAsync(Item);
+        }
+        public void Close()
+        {
+            IsVisible = false;
+            Item = default;
+            EditContext = null;
+
+            StateHasChanged();
+        }
+    }
+}
---
+++ Dnc.Modal.WebApp/Components/Pages/HomeComponent.cs
@@ -0,0 +1,76 @@
+﻿using Dnc.Common.Razor.Modal;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dnc.Modal.WebApp.Components.Pages
+{
+    public class HomeComponent: ComponentBase
+    {
+        protected DncModal<Account> EditModal { get; set; }
+
+        protected DncModal<Account> AddModal { get; set; }
+
+        // Edit Modal Methods
+        protected void EditModalSubmitted(Account account)
+        {
+            var exist = Accounts.FirstOrDefault(v => v.Id == account.Id);
+
+            if (exist != null && EditModal.EditContext.Validate())
+            {
+                // Update the account in real projects
+                EditModal.Close();
+            }
+
+        }
+
+        protected void EditModalDisplayed(Account account)
+        {
+            EditModal.SetEditContext(new EditContext(account));
+        }
+
+        // Add Account Modal Methods
+        protected Account AccountModel { get; set; } = new Account()
+        {
+            Id = Guid.NewGuid().ToString(),
+            ExpireDate = DateTime.Now.AddYears(1)
+        };
+
+        protected void AddModalSubmitted(Account account)
+        {
+            var exist = Accounts.FirstOrDefault(v => v.Id == account.Id);
+
+            if (exist == null && AddModal.EditContext.Validate())
+            {
+                Accounts.Add(account);
+                AddModal.Close();
+            };
+        }
+
+        protected void AddModalDisplayed(Account account)
+        {
+            AddModal.SetEditContext(new EditContext(account));
+        }
+
+        protected List<Account> Accounts = new List<Account>
+    {
+        new Account {Id = Guid.NewGuid().ToString() , Name = "John Smith", Email= "john.smith@coder.com",  Age = 20, ExpireDate = DateTime.Now.AddYears(1)},
+        new Account {Id = Guid.NewGuid().ToString() , Name = "Sarah Johnson", Email= "Sarah.Johnson@coder.com", Age = 30, ExpireDate = DateTime.Now.AddYears(1)},
+        new Account {Id = Guid.NewGuid().ToString() , Name = "Michael Brown", Email= "Michael.Brown@coder.com", Age = 40, ExpireDate = DateTime.Now.AddYears(1)},
+        new Account {Id = Guid.NewGuid().ToString() , Name = "Emily Davis", Email= "Emily.Davis@coder.com", Age = 50, ExpireDate = DateTime.Now.AddYears(1)}
+    };
+    }
+
+    public class Account
+    {
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Insert valid email address")]
+        public string Email { get; set; }
+        [Range(18, 80, ErrorMessage = "The Age should be between 18 and 65 years")]
+        public int Age { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
